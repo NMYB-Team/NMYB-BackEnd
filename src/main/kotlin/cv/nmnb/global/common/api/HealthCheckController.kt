732e7c4194a41f11d5bee07e9f426c5b63bccaa5
yel-m <coin6442@gmail.com>
2025-03-29 04:21:14 +0900

    style : spotless 적용

--- src/main/kotlin/cv/nmnb/global/common/api/HealthCheckController.kt
+++ src/main/kotlin/cv/nmnb/global/common/api/HealthCheckController.kt
@@ -1,6 +1,6 @@
-package cv.nmnb.global.common.api;
+package cv.nmnb.global.common.api
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
